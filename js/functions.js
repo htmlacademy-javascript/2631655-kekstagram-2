bd3cdced2e2cd139a9477bf8f0e7b5f9a5178134
Опознанный енот <you@example.com>
2025-06-17 22:51:25 +0300

    Внёс небольшие правки

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,14 @@
-const checkLengthString = (string, number) => string.length <= number;
+const checkLengthString = (string = '', number = 3) => string.length <= number;
 
-const isPalindrome = (string) => {
+const isPalindrome = (string = '') => {
   const currentString = string.toLowerCase().replaceAll(' ', '');
   let result = '';
+
   for (let i = currentString.length - 1; i >= 0; i--) {
     result += currentString[i];
   }
 
-  if (currentString === result) {
-    return true;
-  }
-  return false;
+  return currentString === result;
 };
 
 checkLengthString();
