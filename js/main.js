0a25ff60534c48766222f7f01c6f6a26d78975c5
Опознанный енот <you@example.com>
2025-07-29 23:01:56 +0300

    Revert "Разрешил конфликт слияния"
    This reverts commit 22983bd1400624d099a4f160f55c1cda926c6abd, reversing
    changes made to f16f48f4191361e6ccdcfdba33a36efb81aabcb8.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import { dataGenerate } from './data.js';
-import { userPhoto } from './user-photos.js';
+import { userPictures } from './user-pictures.js';
 
 
 dataGenerate();
-userPhoto();
+userPictures();
